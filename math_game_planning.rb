4a80aabd89cdd72c15aa7ff2f417ae390bb115bb
Noidea22 <mathewsteenwinkel@gmail.com>
2023-05-07 14:24:29 -0600

    'create game'

---
+++ math_game_planning.rb
@@ -0,0 +1,17 @@
+=begin
+task 1: nouns for classes 
+players
+rounds
+turn
+answers
+game over
+
+task 2: roles
+players - to answer questions being asked of them. have three lives each. 
+rounds - to ask a math questions 
+turn - to determine who is answering next
+answers - if they got the answer correct or wrong and take away a life or not
+game over - to determine who won
+
+
+=end
